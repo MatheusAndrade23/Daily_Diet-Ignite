3eb658983249840b3448fbc46addae2e54fb7ec1
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-12-30 18:01:23 -0300

    Update app.routes.tsx

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,18 +3,23 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "@screens/Home";
 import { Meal } from "@screens/Meal";
 import { NewMeal } from "@screens/NewMeal";
+import { OutDiet } from "@screens/OutDiet";
+import { EditMeal } from "@screens/EditMeal";
+import { InsideDiet } from "@screens/InsideDiet";
+import { GeneralStats } from "@screens/GeneralStats";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export const AppRoutes = () => {
   return (
-    <Navigator
-      initialRouteName="new-meal"
-      screenOptions={{ headerShown: false }}
-    >
+    <Navigator initialRouteName="meal" screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="meal" component={Meal} />
       <Screen name="new-meal" component={NewMeal} />
+      <Screen name="out-diet" component={OutDiet} />
+      <Screen name="edit-meal" component={EditMeal} />
+      <Screen name="inside-diet" component={InsideDiet} />
+      <Screen name="general-stats" component={GeneralStats} />
     </Navigator>
   );
 };
